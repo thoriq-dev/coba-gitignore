1a0082e64790717235fbf371ec56a01b32bf3e8d
thoriq-dev <thoriqtnf07@gmail.com>
2024-01-11 17:07:57 +0700

    Menambahkan empat file baru

---
+++ script.js
@@ -0,0 +1,11 @@
+function namaKamu() {
+  let namaUser = prompt("Siapa Nama Kamu ?");
+  let sapa = "Hi, " + namaUser;
+  return sapa;
+}
+
+alert(namaKamu());
+
+let namaSaya = "Thoriq Nurul Fikri";
+
+console.log(namaSaya);
